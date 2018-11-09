053cc26eae5add0872e02d9ce8cdab2e284333d3
Timothy Johnson <timothy@idioticdev.com>
2018-11-09 15:43:55 -0800

    Correctly track column sizes

--- src/Column.jsx
+++ src/Column.jsx
@@ -1,41 +1,67 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+class RunningMaxValue {
+  #items = {}
+
+  createValue (callback) {
+    let key = Math.random().toString(36).substr(2, 9)
+    this.#items[key] = { callback }
+    return key
+  }
+
+  updateValue (key, value) {
+    this.#items[key].value = value
+    this._propagate()
+  }
+
+  removeValue (key) {
+    delete this.#items[key]
+    this._propagate()
+  }
+
+  _propagate () {
+    let values = Object.values(this.#items)
+    let max = values.reduce((res, o) => Math.max(res, o.value), 0)
+    values.forEach(item => item.callback(max))
+  }
+}
+
 class Column extends Component {
   static propTypes = {
     group: PropTypes.string.isRequired
   }
 
-  static funcs = {}
+  static cache = {}
 
   constructor (props) {
     super(props)
 
-    this.ref = React.createRef()
-
-    let next = Column.funcs[props.group]
-    Column.funcs[props.group] = width => {
+    let value = (Column.cache[props.group] ||= new RunningMaxValue())
+    this.valueKey = value.createValue(width => {
       if (width > this.width && this.ref.current)
         this.ref.current.style.width = width + 'px'
+    })
 
-      next?.(width)
-    }
-
+    this.ref = React.createRef();
     this.componentDidMount = this.componentDidUpdate
   }
 
   render () {
     let { children } = this.props
 
-    return <div ref={ this.ref } className="column" >
+    return <div ref={ this.ref } className="column">
       { children }
     </div>
   }
 
   componentDidUpdate () {
-    let { group } = this.props
     this.width = this.ref.current.clientWidth
-    Column.funcs[group](this.width)
+    Column.cache[this.props.group]?.updateValue(this.valueKey, this.width)
+  }
+
+  componentWillUnmount () {
+    Column.cache[this.props.group]?.removeValue(this.valueKey)
   }
 }
 
